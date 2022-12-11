a5622c0634d228e497ead6e4b41805aee1236bc3
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-12-11 18:58:12 +0200

    Create leetcode(Hard)-124.cpp

---
+++ leetcode/leetcode(Hard)-124.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+      maxPathSum2(root, true);
+      return mx;
+    }
+    int mx = -2000;
+    int maxPathSum2(TreeNode* root, bool is_root) {
+      if (!root) return -2000;
+
+      int m1 = maxPathSum2(root->left, false);
+      int m2 = maxPathSum2(root->right, false);
+      
+      ans = max(root->val, root->val + max(m1, m2));
+
+      
+      mx = max(mx, root->val + m1 + m2);
+      mx = max(mx, ans);
+      mx = max(mx, m1);
+      mx = max(mx, m2);
+
+      return ans;
+    }
+};
